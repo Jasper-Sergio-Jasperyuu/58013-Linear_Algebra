407d68b7d15b339363e857a807e8ca37d24f2e69
Jasper-Sergio-Jasperyuu <123426437+Jasper-Sergio-Jasperyuu@users.noreply.github.com>
2023-11-20 09:04:32 +0800

    Update linear_application.m

--- linear_application.m
+++ linear_application.m
@@ -4,13 +4,13 @@ e1 = 20*x + 10*y == 350; e2 = 17*x + 22*y == 500;
 
 [a, b] = equationsToMatrix([e1, e2], [x, y]);
 
-c = linsolve(a,b)
-
+c = linsolve(a,b);
+c;
 plot(a,b);
-title('Linear Application')
+title('Linear Application');
 grid on;
-xlabel('Apples')
-ylabel('Oranges')
-hold on
-legend('Data Points')
-plot(x, y, '*')
+xlabel('Apples');
+ylabel('Oranges');
+hold on;
+legend('Data Points');
+plot(a, b, '*');
